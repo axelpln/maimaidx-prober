a29aa96414a79c792c770fc636a3cfd07b037e01
AxelPLN（Axel Yinjia Huang） <54173955+axelpln@users.noreply.github.com>
2025-05-18 00:29:48 +0800

    1

--- proxy/cmd/maimaidx-prober-proxy/build.ps1
+++ proxy/cmd/maimaidx-prober-proxy/build.ps1
@@ -1,17 +1,17 @@
 Set-Location -Path $PSScriptRoot
 $version = "1.3.0"
 $filename = "maimaidx-prober-proxy-windows-$version.exe"
-go env -w CGO_ENABLED=1
-go env -w GOOS=windows
-go env -w GOARCH=amd64
+$env:CGO_ENABLED = "1"
+$env:GOOS = "windows"
+$env:GOARCH = "amd64"
 go build -ldflags "-X main.version=$version" -o $filename
 $filename = "maimaidx-prober-proxy-mac-$version"
-go env -w CGO_ENABLED=0
-go env -w GOOS=darwin
-go env -w GOARCH=amd64
+$env:CGO_ENABLED = "0"
+$env:GOOS = "darwin"
+$env:GOARCH = "amd64"
 go build -ldflags "-X main.version=$version" -o $filename
 $filename = "maimaidx-prober-proxy-linux-$version"
-go env -w CGO_ENABLED=0
-go env -w GOOS=linux
-go env -w GOARCH=amd64
+$env:CGO_ENABLED = "0"
+$env:GOOS = "linux"
+$env:GOARCH = "amd64"
 go build -ldflags "-X main.version=$version" -o $filename
